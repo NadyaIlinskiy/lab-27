0d18de7f093028bbc57bb98de2a11f70c2825224
Nadya Ilinskaya <nilinskaya@seattlecca.org>
2020-01-24 18:51:32 -0800

    starter code

--- src/App.js
+++ src/App.js
@@ -1,26 +1,41 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { connect } from 'react-redux';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function App(props) {
+    let formInputs = [];
+
+    if (props.jsonObj.inputs && props.jsonObj.inputs.length)
+        props.jsonObj.inputs.forEach((val, ind) => {
+            formInputs.push(
+                <div key={ind}>Found Input of type {val.type}</div>,
+            );
+        });
+
+    return (
+        <div className='App'>
+            {props.isValid ? <p>VALID JSON!</p> : <p>INVALID JSON!</p>}
+            <textarea
+                id='json-txt'
+                value={props.jsonTxt}
+                onChange={e => {
+                    props.dispatch({
+                        type: 'JSON_CHANGE',
+                        data: e.target.value,
+                    });
+                }}
+            />
+
+            {formInputs}
+        </div>
+    );
 }
 
-export default App;
+const mstp = state => ({
+    jsonTxt: state.json.jsonTxt,
+    isValid: state.json.isValid,
+    jsonObj: state.json.jsonObj,
+});
+
+export default connect(mstp)(App);
